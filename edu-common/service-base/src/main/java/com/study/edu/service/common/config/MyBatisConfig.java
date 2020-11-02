53830a1a25bab7a032e5781d8afae2ffc6fc15b9
RevelationColleciton <310002170@qq.com>
2020-11-02 19:48:50 +0800

    create project

---
+++ edu-common/service-base/src/main/java/com/study/edu/service/common/config/MyBatisConfig.java
@@ -0,0 +1,5 @@
+package com.study.edu.service.common.config;
+
+public class MyBatisConfig {
+
+}
---
+++ edu-service/basis-service/src/main/java/com/study/edu/controller/admin/TeacherController.java
@@ -0,0 +1,16 @@
+package com.study.edu.controller.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/edu/teacher")
+public class TeacherController {
+//    @Autowired
+//    private TeacherService teacherService;
+//    @GetMapping("list")
+//    public List<Teacher> listAll(){
+//        return teacherService.list();
+//    }
+}
